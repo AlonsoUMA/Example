4fe7311c68edee38aa3002723dc5bf24a3a49630
ttrj3 <trejoalexis639@gmail.com>
2024-09-18 18:06:15 -0600

    Update index.php
    Actualización de 2 a 3 números

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 
         <label for="number2">Número 2:</label>
         <input type="number" id="number2" name="number2" required />
+        
+         <label for="number3">Número 3:</label>
+        <input type="number" id="number3" name="number3" required />
 
         <button type="submit">Sumar</button>
     </form>
@@ -21,7 +24,8 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $number1 = $_POST['number1'];
         $number2 = $_POST['number2'];
-        $result = $number1 + $number2;
+        $number3 = $_POST['number3'];
+        $result = $number1 + $number2 $number3;
 
         echo "<h2>Resultado: $result</h2>";
     }
